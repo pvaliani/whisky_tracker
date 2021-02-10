18ff88da779a03a76873fd26a055ab6b1caf5973
Pedram Valiani <p.valiani@gmail.com>
2021-02-10 15:25:38 +0000

    adds getters/setters for whisky model

--- src/main/java/com/example/codeclan/whiskytracker/models/Whisky.java
+++ src/main/java/com/example/codeclan/whiskytracker/models/Whisky.java
@@ -8,6 +8,51 @@ public class Whisky {
     private int age;
     private Distillery distillery;
 
-    
 
+    public Whisky(String name, int year, int age, Distillery distillery) {
+        this.name = name;
+        this.year = year;
+        this.age = age;
+        this.distillery = distillery;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Distillery getDistillery() {
+        return distillery;
+    }
+
+    public void setDistillery(Distillery distillery) {
+        this.distillery = distillery;
+    }
 }
