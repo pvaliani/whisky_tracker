255fd50df7c120b6a0b7d752fa988f8702eead58
Pedram Valiani <p.valiani@gmail.com>
2021-02-10 15:57:37 +0000

    mvp complete

--- src/main/java/com/example/codeclan/whiskytracker/respositories/DistilleryRepository.java
+++ src/main/java/com/example/codeclan/whiskytracker/respositories/DistilleryRepository.java
@@ -4,6 +4,13 @@ import com.example.codeclan.whiskytracker.models.Distillery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DistilleryRepository extends JpaRepository<Distillery, Long> {
+
+    List<Distillery> findByRegion(String region);
+
+    List<Distillery> findByWhiskiesAge(int age);
+
 }
