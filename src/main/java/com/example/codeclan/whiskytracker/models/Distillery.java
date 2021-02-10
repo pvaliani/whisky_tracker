15b3e53c726b405040292b51895d86f7f647ccca
Pedram Valiani <p.valiani@gmail.com>
2021-02-10 15:24:47 +0000

    declare model types/variables and set up given dataloader

--- src/main/java/com/example/codeclan/whiskytracker/models/Distillery.java
+++ src/main/java/com/example/codeclan/whiskytracker/models/Distillery.java
@@ -1,4 +1,13 @@
 package com.example.codeclan.whiskytracker.models;
 
+import java.util.List;
+
 public class Distillery {
+
+    private String name;
+    private String region;
+    private List<Whisky> whiskies;
+
+
+
 }
--- src/main/java/com/example/codeclan/whiskytracker/models/Whisky.java
+++ src/main/java/com/example/codeclan/whiskytracker/models/Whisky.java
@@ -1,4 +1,13 @@
 package com.example.codeclan.whiskytracker.models;
 
 public class Whisky {
+
+    private Long id;
+    private String name;
+    private int year;
+    private int age;
+    private Distillery distillery;
+
+    
+
 }
