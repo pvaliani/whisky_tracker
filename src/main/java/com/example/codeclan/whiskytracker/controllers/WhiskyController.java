a0502c6a519faae3cdc3ac1de093a62aa5f8ea08
Pedram Valiani <p.valiani@gmail.com>
2021-02-10 15:40:16 +0000

    working on whisky repository

--- src/main/java/com/example/codeclan/whiskytracker/controllers/WhiskyController.java
+++ src/main/java/com/example/codeclan/whiskytracker/controllers/WhiskyController.java
@@ -1,4 +1,14 @@
 package com.example.codeclan.whiskytracker.controllers;
 
+import com.example.codeclan.whiskytracker.respositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+
 }
